f9f71805a939aebd7e7259952af5ff12859955f3
rakusan <github@kuramochi.info>
2022-11-24 17:30:39 +0900

    create ios project

---
+++ VPS-UaaL-ios/VPS-UaaL-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VPS-UaaL-ios
+//
+//  Created by Yoshikazu Kuramochi on 2022/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
