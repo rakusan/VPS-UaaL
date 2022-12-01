70f9637682f8b7df2d3ffc148dd5a08597cbd735
rakusan <github@kuramochi.info>
2022-12-01 22:46:48 +0900

    add NativeCallProxy

---
+++ Assets/Plugins/iOS/NativeCallProxy.mm
@@ -0,0 +1,46 @@
+#import <Foundation/Foundation.h>
+#import "NativeCallProxy.h"
+
+
+@implementation FrameworkLibAPI
+
+id<NativeCallsProtocol> api = NULL;
++(void) registerAPIforNativeCalls:(id<NativeCallsProtocol>) aApi
+{
+    api = aApi;
+}
+
+@end
+
+
+extern "C" {
+void updateInfoText(const char* infoText)
+{
+    return [api updateInfoText:[NSString stringWithUTF8String:infoText]];
+}
+
+void updateSnackBarText(const char* snackBarText)
+{
+    return [api updateSnackBarText:[NSString stringWithUTF8String:snackBarText]];
+}
+
+void updateDebugText(const char* debugText)
+{
+    return [api updateDebugText:[NSString stringWithUTF8String:debugText]];
+}
+
+void clearAllButtonSetActive(bool active)
+{
+    return [api clearAllButtonSetActive:active];
+}
+
+void setAnchorButtonSetActive(bool active)
+{
+    return [api setAnchorButtonSetActive:active];
+}
+
+void debugTextSetActive(bool active) {
+    return [api debugTextSetActive:active];
+}
+
+}
